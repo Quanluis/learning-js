9226da9c86f53e3cee91a42e50bc7e8303af494d
Luis Garzon <luisgarzon90@yahoo.com>
2018-01-29 18:55:10 -0500

    made a variable

---
+++ variables-and-datatypes.js
@@ -0,0 +1,12 @@
+// sing line comment 
+/** this is a multi-line 
+ * comment 
+ */ 
+ 
+ //Variables 
+ var name = "Luis"; //old school js (dont use this)
+ const fruit = "banana"; // contant binding (lock it in the cup)
+ let veggie = "Carrot"; //similar to var, but some other differences...
+ console.log("name: ", name); //print
+ console.log("fruit: ", fruit);
+ console.log("veggie:" , veggie);
