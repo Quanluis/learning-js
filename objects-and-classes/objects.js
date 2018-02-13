dabd7feb5088f7a7b3d879d3b356c9fb92a32b81
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-13 18:24:48 -0500

    Created props and objects with circle example

---
+++ objects-and-classes/objects.js
@@ -0,0 +1,24 @@
+// lets create a circle object 
+
+const circle = {
+ radius: 10,
+ color: 'red', 
+ x: 10,
+ y: 10,
+ changeColor: function(newColor){
+     circle.color = newColor;
+ },
+ grow: function(){
+     circle.radius += 1;
+ },
+ shrink: function(){
+     circle.radius -= 1;
+ },
+};
+console.log(circle);
+circle.changeColor("green");
+console.log(circle);
+circle.shrink();
+console.log(circle);
+circle.grow();
+console.log(circle);
